b33e47631c736bc472dd2d824b9a18c3b842cb08
emma-boamah <emmanuelboamah046@gmail.com>
2024-12-18 19:51:23 +0000

    enhanced the security of the server

--- connection.php
+++ connection.php
@@ -1,12 +1,19 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require('/var/www/html/vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 try{
-    $host = "localhost";
-    $user = "root";
-    $pass = "@123ASDubuntu";
-    $dbName = "bytant";
+    $host = $_ENV['DB_HOST'];
+    $user = $_ENV['DB_USER'];
+    $pass = $_ENV['DB_PASS'];
+    $dbName = $_ENV['DB_NAME'];
 
     // DATABASE COMMECTION
-    $db_Connection = new PDO("mysql:host=127.0.0.1; dbname=$dbName; charset=utf8mb4", $user, $pass);
+    $db_Connection = new PDO("mysql:host=$host;dbname=$dbName; charset=utf8mb4", $user, $pass);
     $db_Connection->setAttribute(PDO::ATTR_ERRMODE ,PDO::ERRMODE_EXCEPTION);
     $db_Connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     echo "success";
